e065ed7fc8fec5f4bd8c7f280487bba0da4d1ab0
Ken Valverde <ken.valverde@yahoo.com>
2022-11-25 15:09:50 -0600

    Revamp navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/navBar.vue';
 </script>
 
 <template>
-  <!-- NavBar goes here -->
+  <NavBar></NavBar>
   <!-- SideBar goes here -->
   <div class="mt-3 ml-5">
     <RouterView></RouterView>
